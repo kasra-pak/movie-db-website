48006a74945969101ca7bbcfaaec28a9a178bf2b
kasra paketeghad <kasrapak69@gmail.com>
2022-06-18 12:06:59 +0430

    tailwind customizations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,29 @@
 const reactDom = require("react-dom");
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "./public/index.html",
-],
+  ],
   theme: {
+    screens: {
+      'xs': '500px',
+      ...defaultTheme.screens,
+    },
+
     extend: {
+      fontSize: {
+        'sm-3xl': 'clamp(.875rem, 6vw, 1.875rem)',
+        'xs-2xl': 'clamp(.75rem, 4vw, 1.5rem)',
+      },
       colors: {
-        primary: "#facc15",
+        primary: "#ea580c",
+        secondary: "#1f2937",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+  ],
 }
