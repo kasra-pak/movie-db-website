985f8626e5ac79e39bd45b1c8b960d50e38199dc
kasra paketeghad <kasrapak69@gmail.com>
2022-06-26 16:37:14 +0430

    added a custom horizontal scrollbar

--- src/components/Scroller/index.jsx
+++ src/components/Scroller/index.jsx
@@ -3,7 +3,7 @@ import MovieCard from './MovieCard'
 
 export default function Scroller({ data }) {
   return (
-    <div className='flex gap-4 pt-3 pb-8 px-1 overflow-x-scroll xs:pt-6'>
+    <div className='scroller flex gap-4 pt-3 pb-8 px-1 overflow-x-scroll scroll-smooth touch-pan-x xs:pt-6'>
       {data && data.map(item => (
         <MovieCard key={item.id} data={item} />
       ))}
