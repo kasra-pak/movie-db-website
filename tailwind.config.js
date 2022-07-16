f09f566d41b5dd36ef959baf99b3118cda8a7cf1
kasra paketeghad <kasrapak69@gmail.com>
2022-07-16 16:08:17 +0430

    eslint and prettier config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,18 @@
 const reactDom = require("react-dom");
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     screens: {
-      'xs': '500px',
+      xs: "500px",
       ...defaultTheme.screens,
     },
 
     extend: {
       fontSize: {
-        'sm-3xl': 'clamp(.875rem, 6vw, 1.875rem)',
-        'xs-2xl': 'clamp(.75rem, 4vw, 1.5rem)',
+        "sm-3xl": "clamp(.875rem, 6vw, 1.875rem)",
+        "xs-2xl": "clamp(.75rem, 4vw, 1.5rem)",
       },
       colors: {
         primary: "#ea580c",
@@ -23,7 +20,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/line-clamp'),
-  ],
-}
+  plugins: [require("@tailwindcss/line-clamp")],
+};
