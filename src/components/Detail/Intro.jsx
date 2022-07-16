8c5c267e4d4ee4cad828d60581d0d06a5331164a
kasra paketeghad <kasrapak69@gmail.com>
2022-07-16 19:41:19 +0430

    bug fix: detail page

--- src/components/Detail/Intro.jsx
+++ src/components/Detail/Intro.jsx
@@ -1,56 +1,65 @@
-import React from 'react'
-import { convertToHour } from "../../utils/NumberUtils"
+import React from "react";
+import { convertToHour } from "../../utils/NumberUtils";
 
 import Stars from "../Stars";
-import BookmarkImg from '../../images/mobile-menu/bookmark.svg'
-import { data } from 'autoprefixer';
+import BookmarkImg from "../../images/mobile-menu/bookmark.svg";
 
 export default function Intro({ media, data }) {
+  const Runtime = convertToHour(data.runtime);
+
+  const numberOfEpisodes = `${data.episodes_num} Episode${
+    data.episodes_num > 1 ? "s" : ""
+  }`;
+
+  const firstAndLastRelease =
+    media === "movie"
+      ? data.release
+      : `${data.first_air} - ${data.in_production ? "" : data.last_air}`;
+
   return (
     <>
-      <div className="relative">
+      <div className='relative'>
         <img src={data.backdrop} alt={data.title} />
-        <div className="absolute inset-0 bg-gradient-to-t from-secondary"></div>
+        <div className='absolute inset-0 bg-gradient-to-t from-secondary'></div>
       </div>
-      
-      <div className="relative p-4">
-        <div className="absolute bottom-0 left-[5%] w-3/12">
-          <img src={data.poster} alt={data.title} className="rounded-md" />
-          <div className="absolute w-1/5 top-0 right-[7%] cursor-pointer">
-            <BookmarkImg className="fill-orange-300 stroke-primary stroke-[10px] hover:fill-primary" />
+
+      <div className='relative p-4'>
+        <div className='absolute bottom-0 left-[5%] w-3/12'>
+          <img src={data.poster} alt={data.title} className='rounded-md' />
+          <div className='absolute w-1/5 top-0 right-[7%] cursor-pointer'>
+            <BookmarkImg className='fill-orange-300 stroke-primary stroke-[10px] hover:fill-primary' />
           </div>
         </div>
 
-        <div className="w-8/12 ml-auto">
+        <div className='w-8/12 ml-auto'>
+          <Stars score={data.score.toFixed(1)} />
 
-          <Stars score={(data.score).toFixed(1)} />
-
-          <div className="my-2.5">
-            <h1 className="text-sm-3xl font-semibold capitalize truncate">{data.title}</h1>
-            <p className="text-xs tracking-wider opacity-80 xs:text-sm">{data.tagline}</p>
-          </div>
-
-          <div className="text-slate-300 flex flex-wrap gap-x-2.5 items-center">
-            <p className="text-sm">
-              {media === 'movie' ?
-                data.release :
-                `${data.first_air} - ${data.in_production ? '' : data.last_air}`
-              }
+          <div className='my-2.5'>
+            <h1 className='text-sm-3xl font-semibold capitalize truncate'>
+              {data.title}
+            </h1>
+            <p className='text-xs tracking-wider opacity-80 xs:text-sm'>
+              {data.tagline}
             </p>
-            <div>•</div>
-            <p className="text-sm w-max">{convertToHour(data.runtime)}</p>
-            {media === 'tv' &&
-            <>
-              <div>•</div>
-              <p className="text-sm w-max">
-                {`${data.episodes_num} Episode${data.episodes_num > 1 ? 's' : ''}`}
-              </p>
-            </>
-            }
           </div>
 
+          <div className='text-slate-300 flex flex-wrap gap-x-2.5 items-center'>
+            <p className='text-sm'>{firstAndLastRelease}</p>
+            {Runtime && (
+              <>
+                <div>•</div>
+                <p className='text-sm w-max'>{Runtime}</p>
+              </>
+            )}
+            {media === "tv" && (
+              <>
+                <div>•</div>
+                <p className='text-sm w-max'>{numberOfEpisodes}</p>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </>
-  )
+  );
 }
--- src/utils/NumberUtils.js
+++ src/utils/NumberUtils.js
@@ -1,18 +1,21 @@
 function preventOverflow(num, limit) {
-  if (num === limit)
-    return 0
-  else if (num < 0)
-    return (limit - 1)
-  else 
-    return num
+  if (num === limit) return 0;
+  else if (num < 0) return limit - 1;
+  else return num;
 }
 
 function convertToHour(minutes) {
+  if (typeof minutes !== "number") {
+    return null;
+  }
+
   if (minutes < 60) {
-    return minutes + 'min'
+    return minutes + "min";
   } else {
-    return `${Math.floor(minutes / 60)}h ${minutes % 60 ? `${minutes % 60}min` : ''}` 
+    return `${Math.floor(minutes / 60)}h ${
+      minutes % 60 ? `${minutes % 60}min` : ""
+    }`;
   }
 }
 
-export { preventOverflow, convertToHour }
+export { preventOverflow, convertToHour };
