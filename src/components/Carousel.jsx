9d60d3a84102ccbdc6b6744d6a01b2f984eddeaa
kasra paketeghad <kasrapak69@gmail.com>
2022-07-18 11:28:30 +0430

    init branch

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Carousel({ children }) {
+  return <div className='text-center text-white bg-blue-900'>{children}</div>;
+}
--- src/components/Trends.jsx
+++ src/components/Trends.jsx
@@ -1,35 +1,45 @@
-import React, { useState, useEffect } from 'react'
-import { getTrendingItems } from '../api/functions'
-import ImageSlider from '../components/ImageSlider'
-import LoadingImg from '../images/loading/loading.svg'
-
+import React, { useState, useEffect } from "react";
+import { getTrendingItems } from "../api/functions";
+import ImageSlider from "../components/ImageSlider";
+import LoadingImg from "../images/loading/loading.svg";
+import Carousel from "./Carousel";
 
 export default function Trends() {
-  const [loading, setLoading] = useState(true)
-  const [trends, setTrends] = useState(null)
-  const [activeSlide, setActiveSlide] = useState(0)
+  const [loading, setLoading] = useState(true);
+  const [trends, setTrends] = useState(null);
+  const [activeSlide, setActiveSlide] = useState(0);
 
   useEffect(() => {
     getTrendingItems().then(items => {
-      setTrends(items)
-      setLoading(false)
-    })
-  }, [])
+      setTrends(items);
+      setLoading(false);
+    });
+  }, []);
 
   if (loading)
     return (
-      <section className="aspect-[9/5] flex justify-center items-center">
-        <LoadingImg className="fill-primary w-12 mx-auto" />
+      <section className='aspect-[9/5] flex justify-center items-center'>
+        <LoadingImg className='fill-primary w-12 mx-auto' />
       </section>
-    )
+    );
 
-  return(
-    trends && <section>
-      <ImageSlider
+  return (
+    true && (
+      <section>
+        {/* <ImageSlider
         data={trends}
         activeSlide={activeSlide}
         setActiveSlide={setActiveSlide}
-      />
-    </section>
-  )
+      /> */}
+        <Carousel>
+          <div>slide - 1</div>
+          <div>slide - 2</div>
+          <div>slide - 3</div>
+          <div>slide - 4</div>
+          <button>next</button>
+          <button>prev</button>
+        </Carousel>
+      </section>
+    )
+  );
 }
