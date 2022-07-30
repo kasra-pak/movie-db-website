3f481bb7ba4a402b2259cad0eee163df24ec434f
kasra paketeghad <kasrapak69@gmail.com>
2022-07-30 21:31:22 +0430

    modal dismiss when click outside

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,16 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Modal({ show, children }) {
-  // const [isOpen, setIsOpen] = useState("false");
+  const [isOpen, setIsOpen] = useState(true);
 
-  ////
-  return show ? (
-    <div className='fixed inset-0 top-[47px] bg-[rgba(0,0,0,.6)] flex justify-center items-start px-4 z-30 scroller overflow-y-scroll sm:top-[59px]'>
+  useEffect(() => {
+    if (show) {
+      setIsOpen(true);
+    }
+  }, [show]);
+
+  const dismiss = () => setIsOpen(prevState => !prevState);
+
+  console.log(isOpen);
+
+  return isOpen && show ? (
+    <div
+      onClick={dismiss}
+      className='fixed inset-0 top-[47px] bg-[rgba(0,0,0,.6)] flex justify-center items-start px-4 z-30 scroller overflow-y-scroll sm:top-[59px]'
+    >
       <div className='bg-slate-600 w-full max-w-md h-[min(80%,_fit-content)] min-h-[7rem] mt-4 p-6 rounded-md md:mt-20'>
         {children}
       </div>
     </div>
   ) : null;
 }
-
-// shadow-[0_0_0_max(100vw,_100vh)_rgba(0,0,0,0.75)]
