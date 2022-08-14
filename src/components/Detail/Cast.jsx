e3b88a182b983917152b7d973080c33b7449dfdc
kasra paketeghad <kasrapak69@gmail.com>
2022-08-14 15:44:54 +0430

    Cast Section: Added infinite scroll

--- src/components/Detail/Cast.jsx
+++ src/components/Detail/Cast.jsx
@@ -1,59 +1,107 @@
-import React, { useState, useEffect } from 'react'
-import { getCast } from '../../api/functions'
-import LoadingImg from '../../images/loading/loading.svg'
+import React, { useState, useEffect, useRef } from "react";
+import { getCast } from "../../api/functions";
+import LoadingImg from "../../images/loading/loading.svg";
+import Spinner from "../../images/loading/spinner.svg";
 
-export default function Cast({ media, id}) {
-  const [loading, setLoading] = useState(true)
-  const [showMore, setShowMore] = useState(false)
-  const [cast, setCast] = useState(null)
+export default function Cast({ media, id }) {
+  const [loading, setLoading] = useState(true);
+  const [cast, setCast] = useState([]);
+
+  const [passed, setPassed] = useState(false);
+  const initialLimit = 6;
+  const [limit, setLimit] = useState(initialLimit);
+  const castElRef = useRef();
 
   useEffect(() => {
     getCast(media, id).then(data => {
-      setCast(data)
-      setLoading(false)
-    })
-  }, [])
+      setCast(data);
+      setLoading(false);
+    });
+  }, []);
 
-  function toggleShowMore() {
-    setShowMore(prevState => !prevState)
+  useEffect(() => {
+    if (passed) {
+      setTimeout(() => {
+        setLimit(prevLimit =>
+          prevLimit + 10 < cast.length ? prevLimit + 10 : cast.length
+        );
+      }, 1000);
+    }
+  }, [passed]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  function handleScroll() {
+    const castElBottom = castElRef.current.getBoundingClientRect().bottom;
+
+    if (window.innerHeight > castElBottom) {
+      if (!passed) {
+        console.log("here");
+
+        setPassed(true);
+      }
+    } else {
+      setPassed(false);
+    }
   }
 
-  let castEl
+  function collapseList() {
+    setLimit(initialLimit);
+    window.scrollTo(0, castElRef.current.getBoundingClientRect().bottom);
+  }
+
+  let castEl;
   if (!loading) {
     castEl = cast.map((item, idx) => (
-      <div key={idx} className="bg-slate-700 flex items-center gap-3 rounded-lg overflow-hidden shadow-md">
+      <div
+        key={idx}
+        className='bg-slate-700 flex items-center gap-3 rounded-lg overflow-hidden shadow-md'
+      >
         <img src={item.profile} alt={item.name} className='w-20' />
         <p>
-          <span className='text-slate-100 font-semibold tracking-wider'>{item.name}</span>
+          <span className='text-slate-100 font-semibold tracking-wider'>
+            {item.name}
+          </span>
           <br />
-          <span className="text-slate-300">
+          <span className='text-slate-300'>
             as
             <br />
             {item.character}
           </span>
         </p>
       </div>
-    ))
+    ));
   }
 
+  return loading ? (
+    <div className='aspect[9/5] flex justify-center items-center'>
+      <LoadingImg className='w-12 fill-primary' />
+    </div>
+  ) : (
+    <div
+      ref={castElRef}
+      className='grid grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] gap-4'
+    >
+      {castEl.slice(0, limit)}
 
-  return (
-    loading ?
-      <div className="aspect[9/5] flex justify-center items-center">
-        <LoadingImg className="w-12 fill-primary" />
-      </div> :
+      {cast.length > initialLimit && cast.length > limit && (
+        <div className='col-span-full justify-self-center'>
+          <Spinner className='w-12 fill-primary animate-spin' />
+        </div>
+      )}
 
-      <div className="grid grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] gap-4">
-        {showMore ? castEl : castEl.slice(0, 6)}
-
-        {cast.length > 6 &&
-          <button
-          onClick={toggleShowMore}
-          className='bg-orange-500 capitalize font-semibold tracking-wider min-w-max w-1/4 max-w-xs justify-self-center col-span-full px-4 py-1  rounded-lg shadow-md hover:bg-primary'>
-            {showMore ? 'show less' : 'show more'}
-          </button>
-        }
-      </div>
-    
-  )
+      {limit === cast.length && limit > initialLimit && (
+        <button
+          onClick={collapseList}
+          className='bg-orange-500 capitalize font-semibold tracking-wider min-w-max w-1/4 max-w-xs justify-self-center col-span-full px-4 py-1  rounded-lg shadow-md hover:bg-primary'
+        >
+          collapse all
+        </button>
+      )}
+    </div>
+  );
 }
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,45 +1,48 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getDetail } from "../api/functions"
+import { getDetail } from "../api/functions";
 
-import Navbar from "../components/Navbar"
-import Intro from "../components/Detail/Intro"
-import InfoSection from "../components/Detail/InfoSection"
+import Navbar from "../components/Navbar";
+import Intro from "../components/Detail/Intro";
+import InfoSection from "../components/Detail/InfoSection";
 import Cast from "../components/Detail/Cast";
 
-import LoadingImg from '../images/loading/loading.svg'
+import LoadingImg from "../images/loading/loading.svg";
+import Footer from "../components/Shared/Footer";
 
 export default function Detail() {
-  const [loading, setLoading] = useState(true)
-  const [data, setData] = useState(null)
-  const {media, id} = useParams()
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(null);
+  const { media, id } = useParams();
 
   useEffect(() => {
     getDetail(media, id).then(data => {
-      setData(data)
-      setLoading(false)
-    })
-  }, [])
+      setData(data);
+      setLoading(false);
+    });
+  }, []);
 
   return (
     <>
       <Navbar />
-      {loading ?
-        <main className="min-h-[90vh] flex justify-center items-center">
-          <LoadingImg className="w-12 fill-primary" />
-        </main> :
-        
-        <main className="bg-secondary text-gray-100">
+      {loading ? (
+        <main className='min-h-[90vh] flex justify-center items-center'>
+          <LoadingImg className='w-12 fill-primary' />
+        </main>
+      ) : (
+        <main className='bg-secondary text-gray-100'>
           <Intro media={media} data={data} />
 
           <InfoSection SectionHeader='Storyline'>
             <p>{data.overview}</p>
           </InfoSection>
 
-          <InfoSection SectionHeader={'cast'}>
-            <Cast {...{media, id}} />
+          <InfoSection SectionHeader={"cast"}>
+            <Cast {...{ media, id }} />
           </InfoSection>
-        </main>}
+        </main>
+      )}
+      <Footer />
     </>
-  )
+  );
 }
