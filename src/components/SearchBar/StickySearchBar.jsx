8c882627e875af33e0303593792775414dab376e
kasra paketeghad <kasrapak69@gmail.com>
2022-08-18 16:33:29 +0430

    Refactored sticky searchbar

--- src/components/SearchBar/StickySearchBar.jsx
+++ src/components/SearchBar/StickySearchBar.jsx
@@ -1,46 +1,37 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { useSearchContext } from "../../contexts/SearchContext";
 import SearchBar from "./index";
 
 export default function StickySearchBar() {
   const { searchBarOpen, searchTerm } = useSearchContext();
-  const [searchBarTop, setSearchBarTop] = useState(null);
-  const [navbarHeight, setNavbarHeight] = useState(null);
+  const [isPassed, setIsPassed] = useState(false);
   const searchEl = useRef();
   const lineEl = useRef();
 
   useEffect(() => {
-    setValues();
-  }, []);
+    window.addEventListener("scroll", checkPassed);
+
+    return () => window.removeEventListener("scroll", checkPassed);
+  }, [checkPassed, searchBarOpen, searchTerm]);
 
   useEffect(() => {
-    window.addEventListener("resize", setValues);
-
-    return () => window.removeEventListener("resize", setValues);
-  }, []);
-
-  useEffect(() => {
-    if (!searchBarTop) return;
     checkStick();
-    window.addEventListener("scroll", checkStick);
+  }, [checkStick, isPassed, searchBarOpen, searchTerm]);
 
-    return () => window.removeEventListener("scroll", checkStick);
-  }, [searchBarTop, searchBarOpen, searchTerm]);
-
-  function setValues() {
+  const checkPassed = useCallback(() => {
     const topValue = searchEl.current.offsetTop;
     const navbarHeightValue = document.querySelector("nav").offsetHeight;
 
-    if (topValue <= navbarHeightValue) return;
-    if (searchEl.current.classList.contains("sticky")) return;
+    const searchBarPassed = window.scrollY + 1 >= topValue - navbarHeightValue;
 
-    setSearchBarTop(topValue);
-    setNavbarHeight(navbarHeightValue);
-  }
+    if (searchBarOpen && searchTerm && searchBarPassed) {
+      return;
+    }
 
-  function checkStick() {
-    const searchBarPassed = window.scrollY > searchBarTop - navbarHeight;
+    setIsPassed(searchBarPassed);
+  }, [searchBarOpen, searchTerm]);
 
+  const checkStick = useCallback(() => {
     // "sticky-searchbar", "fix-to-top" and "stick" are custom classes
     const baseClassNames = `sticky-searchbar ${
       searchBarOpen
@@ -50,7 +41,7 @@ export default function StickySearchBar() {
 
     let finalClassNames = "";
 
-    if (searchBarPassed) {
+    if (isPassed) {
       lineEl.current.classList.add("hidden");
       finalClassNames = `${baseClassNames} stick`;
     } else {
@@ -64,10 +55,10 @@ export default function StickySearchBar() {
     }
 
     searchEl.current.classList = finalClassNames;
-  }
+  }, [isPassed, searchBarOpen, searchTerm]);
 
   return (
-    <div ref={searchEl} className='opacity-0'>
+    <div ref={searchEl} className=''>
       <div
         ref={lineEl}
         className='absolute w-96 top-1/2 -translate-y-1/2 h-1 bg-gradient-to-l from-transparent via-primary to-transparent'
