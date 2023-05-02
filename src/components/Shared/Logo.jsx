3063f4443228b5ae31e30e7d17dc353646106edb
kasra paketeghad <kasrapak69@gmail.com>
2023-05-02 07:24:23 +0330

    Link logo to homepage

--- src/components/Shared/Logo.jsx
+++ src/components/Shared/Logo.jsx
@@ -1,18 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Logo() {
   return (
-    <>
-      <span className='flex items-baseline gap-x-px text-sm font-bold uppercase '>
-        movie
-        <svg
-          className='aspect-square w-1 fill-smashingPumpkins'
-          viewBox='0 0 32 32'
-          xmlns='http://www.w3.org/2000/svg'
-        >
-          <circle cx='16' cy='16' r='16' />
-        </svg>
-      </span>
-    </>
+    <Link
+      to='/'
+      className='flex items-baseline gap-x-px text-sm font-bold uppercase'
+    >
+      movie
+      <svg
+        className='aspect-square w-1 fill-smashingPumpkins'
+        viewBox='0 0 32 32'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <circle cx='16' cy='16' r='16' />
+      </svg>
+    </Link>
   );
 }
