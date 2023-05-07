4d9ff5bd3300a904d0368c4639625eca4862e281
kasra <kasrapak69@gmail.com>
2023-05-07 12:31:27 +0330

    hide scrollbar

--- src/components/Scroller/index.jsx
+++ src/components/Scroller/index.jsx
@@ -3,7 +3,7 @@ import MovieCard from "./MovieCard";
 
 export default function Scroller({ data }) {
   return (
-    <div className='scroller flex touch-pan-x gap-4 overflow-y-visible overflow-x-scroll scroll-smooth px-1 pb-8 pt-3 xs:pt-6'>
+    <div className='scrollbar-hidden flex gap-4 overflow-x-scroll px-1 pb-8 pt-3 xs:pt-6'>
       {data && data.map(item => <MovieCard key={item.id} data={item} />)}
     </div>
   );
