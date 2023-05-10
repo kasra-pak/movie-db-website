ef1a736b67f96aa27203d662549a32d64e58ac67
kasra <kasrapak69@gmail.com>
2023-05-10 17:02:55 +0330

    Focus on input when the search bar is open or clicked

--- src/components/Header/SearchBar.jsx
+++ src/components/Header/SearchBar.jsx
@@ -1,13 +1,19 @@
 import React, { useEffect, useRef } from "react";
 
 const SearchBar = ({ isOpen, closeSearchBar }) => {
-  const nodeRef = useRef(null);
+  const searchBarRef = useRef(null);
 
   useEffect(() => {
-    const checkDismiss = e => {
+    if (isOpen) {
+      searchBarRef.current.querySelector("#search-input").focus();
+    }
+  }, [isOpen]);
+
+  useEffect(() => {
+    const dismiss = e => {
       if (
-        nodeRef.current &&
-        !nodeRef.current.contains(e.target) &&
+        searchBarRef.current &&
+        !searchBarRef.current.contains(e.target) &&
         e.target.id !== "search-toggle-button"
       ) {
         closeSearchBar();
@@ -15,19 +21,27 @@ const SearchBar = ({ isOpen, closeSearchBar }) => {
     };
 
     if (isOpen) {
-      document.addEventListener("click", checkDismiss);
+      document.addEventListener("click", dismiss);
     }
 
     return () => {
-      document.removeEventListener("click", checkDismiss);
+      document.removeEventListener("click", dismiss);
     };
   }, [closeSearchBar, isOpen]);
 
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
   return (
-    <div
-      ref={nodeRef}
+    <form
+      onSubmit={handleSubmit}
+      ref={searchBarRef}
+      onClick={e => {
+        e.currentTarget.querySelector("#search-input").focus();
+      }}
       id='search-bar'
-      className={`fixed inset-x-0 z-20 flex justify-between bg-white/70 px-6 py-3 backdrop-blur-[6px] ${
+      className={`fixed inset-x-0 z-20 flex cursor-text justify-between bg-white/70 px-6 py-3 backdrop-blur-[6px] ${
         isOpen ? "fixed" : "-translate-y-full"
       }`}
     >
@@ -40,13 +54,13 @@ const SearchBar = ({ isOpen, closeSearchBar }) => {
         >
           <path d='m29 27.586-7.552-7.552a11.018 11.018 0 1 0-1.414 1.414L27.586 29ZM4 13a9 9 0 1 1 9 9 9.01 9.01 0 0 1-9-9Z' />
         </svg>
-        <label htmlFor='search' className='sr-only'>
+        <label htmlFor='search-input' className='sr-only'>
           Search
         </label>
         <input
           type='search'
           name='search'
-          id='search'
+          id='search-input'
           placeholder='Search...'
           className='flex-grow bg-inherit text-sm font-bold text-midnightExpress outline-none'
         />
@@ -54,7 +68,7 @@ const SearchBar = ({ isOpen, closeSearchBar }) => {
       <button className='rounded-lg bg-smashingPumpkins px-3 py-2 text-sm font-bold text-white hover:bg-lobster hover:shadow-pumpkins'>
         Search
       </button>
-    </div>
+    </form>
   );
 };
 
